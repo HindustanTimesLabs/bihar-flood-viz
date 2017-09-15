8e06879e6dac169bfeb74a859e63807126816b8d
HarryStevens <harryjosephstevens@gmail.com>
2017-09-15 17:57:24 +0530

    Add small multiples

--- js/scripts_deaths.js
+++ js/scripts_deaths.js
@@ -1,19 +1,29 @@
-var start_year = 2007;
+var start_year = 2006, end_year = 2017;
 
 var isMobile = false; //initiate as false
-// device detection
-if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
-
 
 var ww = $(window).width(),
   resp = {};
 
 if (ww <= 768){
-  // resp.
+  // mobile
+  isMobile = true;
+  resp.height = 250;
+  resp.range_max = 25;
+  resp.leg_height = 55;
+  resp.leg_width = 80;
+  resp.width = window.innerWidth;
+} else {
+  // desktop
+  isMobile = false;
+  resp.height = 250;
+  resp.width = (window.innerWidth / 4) * .8;
+  resp.range_max = 25;
+  resp.leg_height = 55;
+  resp.leg_width = 80;
 }
 
-var width = window.innerWidth, height = 600;
+var width = resp.width, height = resp.height;
 
 var projection = d3.geoMercator();
 
@@ -25,15 +35,15 @@ var svg = d3.select(".map-wrapper.district-deaths .map").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var legend_deaths_width = 170,
-  legend_deaths_height = 105;
+var legend_deaths_width = resp.leg_width,
+  legend_deaths_height = resp.leg_height;
 
 var legend_deaths = d3.select(".map-wrapper.district-deaths .legend-death-circles").append("svg")
     .attr("width", legend_deaths_width)
     .attr("height", legend_deaths_height);
 
 var death_circle_scale = d3.scaleLinear()
-    .range([0, 50]);
+    .range([0, resp.range_max]);
 
 d3.queue()
   .defer(d3.json, "data/bihar_district.json")
@@ -59,31 +69,53 @@ function ready(error, district, state, city, state_label, deaths){
   // set up death circle scale domain based on min and max deaths
   death_circle_scale.domain(d3.extent(deaths, function(d){ return +d.deaths; }))
 
-  redraw(start_year, svg);
-
   drawLegendDeaths(deaths);
 
   function redraw(yr, svg){
-    $(".map-wrapper.district-deaths .legend-date").html(yr);
+    if (isMobile) $(".map-wrapper.district-deaths .legend-date").html(yr);
     drawDeathCircles(district, filterDeathData(deaths, yr), svg);
-    fillSubUnits("district", filterDeathData(deaths, start_year), svg);
+    fillSubUnits("district", filterDeathData(deaths, yr), svg);
   }
 
   // gif
   if (isMobile) {
+    redraw(start_year, svg)
     d3.interval(function(){
-      if (start_year == 2017){
-        start_year = 2007
+      if (start_year == end_year){
+        start_year = 2006
       } else {
         ++start_year
       }
       redraw(start_year, svg);
     }, 1000);
-  }
+  } else {
+    // small multiples
+    var svg_obj = {};
+    for (var i = start_year; i <= end_year; i++){
 
+      $(".small-mult-wrapper").append("<div class='map map-small-mult map-" + i + "'></div>");
+      $(".small-mult-wrapper .map-" + i).append("<div class='legend-date'>" + i + "</div>");
+
+      var year_data = filterDeathData(deaths, i),
+        districts = year_data.filter(function(d){ return d.deaths != "0"; }).length,
+        total_deaths = d3.sum(year_data, function(d){ return +d.deaths; });
+      $(".small-mult-wrapper .map-" + i).append("<div class='legend-sentence-small'>" + jz.str.numberLakhs(total_deaths) + " killed in <span class='red-text'>" + districts + "</span> districts.</div>")
+
+      svg_obj[i] = d3.select(".map-wrapper.district-deaths .small-mult-wrapper .map-small-mult.map-" + i).append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+      centerZoom(district, svg_obj[i]);
+      centerZoom(district, svg_obj[i]);
+      drawSubUnits(district, "district", svg_obj[i]);
+      var boundary = centerZoom(district, svg_obj[i]);
+      drawOuterBoundary(district, boundary, svg_obj[i]);
+      redraw(i, svg_obj[i]);
+    }
+  }
 }
 
-function fillSubUnits(cl, data, svg){
+function fillSubUnits(cl, data, svg, yr){
 
   var count = 0,
     deaths = 0;
@@ -104,8 +136,6 @@ function fillSubUnits(cl, data, svg){
       });
 
   $(".legend-sentence").html("<b>" + jz.str.numberLakhs(deaths) + "</b> people were killed in <span class='red-text'><b>" + count + "</b></span> districts.");
-  
-
 
   function lookup_data(d){
     return data.filter(function(row){
@@ -144,7 +174,7 @@ function drawLegendDeaths(data){
       .attr("class", "legend-daily-number legend-number")
       .attr("x", death_circle_scale(m) * 2 + 10)
       .attr("y", function(d){ return death_circle_scale(d * 2) + 5; })
-      .text(function(d, i){ return d + (i == 2 ? " deaths" : ""); });
+      .text(function(d, i){ return d + (i == 2 ? "" : ""); });
 
 }
 
